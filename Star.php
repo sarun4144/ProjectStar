09a1421b3536a2d7f1757f8586288b76c9d5ddcb
sarun4144 <92587552+sarun4144@users.noreply.github.com>
2021-11-11 21:43:25 +0700

    Add files via upload

--- Star.php
+++ Star.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="">
 <head>
-</style>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="BG.css">
+    <link rel="stylesheet" href="Animation.css">
     <link href='https://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js" type="text/javascript"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
@@ -16,22 +16,20 @@
     <link rel="icon" href="https://scontent.xx.fbcdn.net/v/t1.15752-9/s261x260/245799487_267531408630886_2729645764553798750_n.png?_nc_cat=111&ccb=1-5&_nc_sid=aee45a&_nc_ohc=SdR8eRCLh24AX8iffW2&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=5bcf13950dee4473d137c76a46870f42&oe=61911213">
 </head>
 <body>
+
+<?php include 'navbar.php';?>
 <div id="stars"></div>
 <div id="stars2"></div>
 <div id="stars3"></div>
-  <div class="container">
-      <?php include 'navbar.php';?>
-      <div id="title"></div>
-      <div class="center">
-        <br><br><br><br><br><br><br><br>
-      <h1 class="display-1">
-        <span>SpaceUtopia</span>
-        <span>SpaceUtopia</span>
-      </h1>
-        <h2 class="display-3">Marketplace</h2>
-      </div>
-      
-      
-  </div>
+<div class="center">
+<div class="lines"></div>
+<h1>
+  <span>SpaceUtopia</span>
+  <span>SpaceUtopia</span>
+</h1>
+<h2 >Marketplace</h2>
+</div>
+<div class="Ani1"><img src="Cult.png" ></div>
+<div id="cat"></div>
 </body>
 </html>
--- navbar.php
+++ navbar.php
@@ -1,20 +1,19 @@
-<link rel="stylesheet" href="BG.css">
-<header>
-      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start" style="background-color:#090629 ;">
+<link rel="stylesheet" href="BG.css"> 
+<header class="p-1 text-white sticky-top">
+      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
         <a href="Star.php"><img src="https://scontent.xx.fbcdn.net/v/t1.15752-9/s261x260/245799487_267531408630886_2729645764553798750_n.png?_nc_cat=111&ccb=1-5&_nc_sid=aee45a&_nc_ohc=SdR8eRCLh24AX8iffW2&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=5bcf13950dee4473d137c76a46870f42&oe=61911213" width="auto" height="60px"></a>
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-          <li><a href="#" class="White nav-link px-2 fw-bolder" title="DashBoard"><i class="bi bi-window me-2"></i>Dashboard</a></li>
-          <li><a href="#" class="White nav-link px-2 fw-bolder" title="Market"><i class="bi bi-shop-window me-2"></i>Market</a></li>
+          <li tabindex="0"><a href="#" class="White nav-link px-2 fw-bolder" title="DashBoard" tabindex="-1" ><i class="bi bi-window me-2"></i>Dashboard</a></li>
+          <li tabindex="0"><a href="#" class="White nav-link px-2 fw-bolder" title="Market"  tabindex="-1"><i class="bi bi-shop-window me-2"></i>Market</a></li>
         </ul>
+
         <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
           <input type="search" class="form-control form-control-dark" placeholder="Search..." aria-label="Search">
         </form>
-        <a href="login.html">
-          <div div class="text-end">
-            <button type="button" class="btn text-white fw-bold button-h" style="background-color:#E95589">Login</button>
-          </div>
-        </a>
-          
-        
+
+        <div class="text-end">
+          <button type="button" class="btn btn-outline-light me-2 fw-bold">Login</button>
+          <button type="button" class="btn text-white fw-bold" style="background:#E95589">Sign-up</button>
+        </div>
       </div>
   </header>
