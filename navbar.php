6224de524ab7f23bdafd4c639dc572a6bc7cce50
DSAQWERTY001 <92746239+DSAQWERTY001@users.noreply.github.com>
2021-11-11 16:30:19 +0700

    Add files via upload

--- navbar.php
+++ navbar.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" href="BG.css">
-<header class="sticky-top">
+<header>
       <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start" style="background-color:#090629 ;">
         <a href="Star.php"><img src="https://scontent.xx.fbcdn.net/v/t1.15752-9/s261x260/245799487_267531408630886_2729645764553798750_n.png?_nc_cat=111&ccb=1-5&_nc_sid=aee45a&_nc_ohc=SdR8eRCLh24AX8iffW2&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=5bcf13950dee4473d137c76a46870f42&oe=61911213" width="auto" height="60px"></a>
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
@@ -9,9 +9,12 @@
         <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
           <input type="search" class="form-control form-control-dark" placeholder="Search..." aria-label="Search">
         </form>
-        <div class="text-end">
-          <button type="button" class="btn btn-outline-light me-2 fw-bold">Login</button>
-          <button type="button" class="btn text-white fw-bold" style="background-color:#E95589">Sign-up</button>
-        </div>
+        <a href="login.html">
+          <div div class="text-end">
+            <button type="button" class="btn text-white fw-bold button-h" style="background-color:#E95589">Login</button>
+          </div>
+        </a>
+          
+        
       </div>
   </header>
