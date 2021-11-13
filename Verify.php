b7017b6d88675f87fe96d49d9f9607e13bf9bcff
Ing9147 <85999451+Ing9147@users.noreply.github.com>
2021-11-13 22:47:01 +0700

    Add files via upload

---
+++ Verify.php
@@ -0,0 +1,25 @@
+<?php 
+ session_start();
+ if(isset($_SESSION["Username"]) && $_SESSION["id"]==session_id()){
+     header("location:Star.php");
+     die();
+ }
+    $u = $_POST['Username'];
+    $p = $_POST['pass'];
+    $conn = new PDO("mysql:host=localhost;dbname=spaceutopia;charset=utf8","root","");
+    $sql = "select * from user where Username = '$u' and Pass = sha1('$p') ";
+    $result =  $conn -> query($sql);
+    if($result -> rowCount() == 1){
+        $data = $result -> fetch(PDO::FETCH_ASSOC);
+        $_SESSION["Username"] = $data["Username"];
+        $_SESSION["UserID"] = $data["ID"];
+        $_SESSION["id"] = session_id();
+        header("location:Star.php");
+        die();
+    }else{
+        $_SESSION["error"] = 1;
+        header("location:login.php");
+        die();
+    }
+    $conn = null;
+?>
